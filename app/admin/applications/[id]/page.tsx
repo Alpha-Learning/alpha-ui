ab8e5536ddc92e7681f49533393778c1bf4283c3
Anurag MK <anurag@syinnovation.co>
2025-09-26 12:59:58 +0530

    fifth stage completed

--- app/admin/applications/[id]/page.tsx
+++ app/admin/applications/[id]/page.tsx
@@ -168,6 +168,7 @@ export default function AdminApplicationDetailPage() {
               1: `/admin/applications/${data.id}`,
               2: `/admin/applications/${data.id}/screening-call`,
               4: `/admin/applications/${data.id}/facility-walkthrough-checklist`,
+              5: `/admin/applications/${data.id}/initial-observation-form`,
             };
             const href = hrefMap[stageNumber];
             
