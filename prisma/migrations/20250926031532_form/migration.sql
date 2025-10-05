872bf2d2767d42dac042992dd466a31a9340a282
Anurag MK <anurag@syinnovation.co>
2025-10-05 11:03:27 +0530

    removed migrations

--- prisma/migrations/20250926031532_form/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."screening_calls" ADD COLUMN     "loggedBy" TEXT,
-ADD COLUMN     "loggedToSystemDate" TEXT,
-ADD COLUMN     "overviewNotes" TEXT;
--- prisma/migrations/20251001015600_add_individual_questionnaire_flags/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."applications" ADD COLUMN     "isCaregiverFormCompleted" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "isOutsiderFormCompleted" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "isParentGuardianFormCompleted" BOOLEAN NOT NULL DEFAULT false;
